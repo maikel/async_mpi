d7a53242834438bab9da068fa33a3b706f9078af
Maikel Nadolski <maikel.nadolski@gmail.com>
2021-03-17 12:53:43 +0000

    Working bulk_finally example

--- include/ampi/bulk_finally.hpp
+++ include/ampi/bulk_finally.hpp
@@ -30,8 +30,9 @@ using get_scheduler_t = decltype(unifex::get_scheduler(std::declval<Receiver&>()
 template <typename SourceManySender, typename SenderFactory, typename ManyReceiver>
 struct operation_state {
   struct operation_base {
+    operation_base() = default;
     virtual ~operation_base() = default;
-    operation_base* next;
+    operation_base* next{};
   };
 
   // This class receives next values from a ManySender source.
@@ -46,19 +47,20 @@ struct operation_state {
   // TODO: If set_error or set_done is called by a single sender this class calls std::terminate.
   struct single_sender_to_many_receiver;
 
-  [[no_unique_address]] SourceManySender source;
   [[no_unique_address]] SenderFactory factory;
   [[no_unique_address]] ManyReceiver many_receiver;
+
+  // We store the operation state of the parent in an optional which does not require a heap
+  // allocation.
+  unifex::connect_result_t<SourceManySender, many_receiver_to_single_sender> parent_operation;
+
+  operation_state(SourceManySender&& source, SenderFactory&& f, ManyReceiver&& r);
 
   // This counts all active operations and will be increased by every created operation state, the
   // parent and all its continuations. It will also be decreased upon destruction of an operation
   // state. The operation that decreases this back to 0 is responsible for deallocating all
   // allocated operation states.
   std::atomic<std::size_t> n_operation_states{0};
-
-  // We store the operation state of the parent in an optional which does not require a heap
-  // allocation.
-  std::optional<operation_t<SourceManySender, many_receiver_to_single_sender>> parent_operation;
 
   // Storage for each operation state of a continuation.
   // This will be dequeued and deallocated by the last active operation.
@@ -99,13 +101,13 @@ struct operation_state {
         std::allocator_traits<ChildBaseAllocator>::destroy(allocator, pointer);
         std::allocator_traits<ChildBaseAllocator>::deallocate(allocator, pointer, 1);
       }
+      unifex::set_value(std::move(many_receiver));
     }
   }
 
   void start() & noexcept {
-    parent_operation = unifex::connect(std::move(source), many_receiver_to_single_sender{this});
     n_operation_states = 1;
-    parent_operation->start();
+    parent_operation.start();
   }
 };
 
@@ -114,10 +116,9 @@ struct operation_state<SourceManySender, SenderFactory, ManyReceiver>::
     single_sender_to_many_receiver {
   operation_state<SourceManySender, SenderFactory, ManyReceiver>* op;
 
-  template <typename R, typename... Args>
-    requires unifex::is_next_receiver_v<std::remove_cvref_t<R>>
-  void set_value(R& receiver, Args&&... args) && noexcept {
-    unifex::set_next(receiver, std::forward<Args>(args)...);
+  template <typename... Args>
+  void set_value(Args&&... args) && noexcept {
+    unifex::set_next(op->many_receiver, std::forward<Args>(args)...);
     op->decrease_operation_count_and_deallocate_all_children_if_last();
   }
 
@@ -151,7 +152,7 @@ struct operation_state<SourceManySender, SenderFactory, ManyReceiver>::
     if constexpr (!std::is_void_v<std::invoke_result_t<SenderFactory, Args...>>) {
       assert(op);
       std::apply(
-          [&](auto&&... senders) { (op->enqueue_continuation(senders), ...); },
+          [&](auto&&... senders) { (op->enqueue_continuation(std::move(senders)), ...); },
           std::tuple{std::invoke(op->factory, std::forward<Args>(args)...)});
     }
   }
@@ -180,22 +181,28 @@ void operation_state<SourceManySender, SenderFactory, ManyReceiver>::enqueue_con
   // Type erasure for the newly enqueued operation state.
   struct operation : operation_base {
     operation(Continuation&& s, single_sender_to_many_receiver&& r) noexcept
-      : operation_base{nullptr}
-      , operation_state{unifex::connect(std::move(s), std::move(r))} {}
-    operation_t<Continuation, single_sender_to_many_receiver> operation_state;
-    void start() & noexcept { operation_state.start(); }
+      : operation_base()
+      , op{unifex::connect(std::move(s), std::move(r))} {}
+    operation_t<Continuation, single_sender_to_many_receiver> op;
+    void start() & noexcept { op.start(); }
   };
   using ChildAllocator = rebind_alloc_t<unifex::get_allocator_t<ManyReceiver>, operation>;
   ChildAllocator child_allocator = rebind_alloc<operation>(get_allocator());
   auto new_child = std::allocator_traits<ChildAllocator>::allocate(child_allocator, 1);
   n_operation_states.fetch_add(1);
   std::allocator_traits<ChildAllocator>::construct(
-      child_allocator,
-      new_child,
-      std::move(continuation),
-      single_sender_to_many_receiver{std::addressof(*parent_operation)});
-  operation_states_of_children.enqueue(new_child);
+      child_allocator, new_child, std::move(continuation), single_sender_to_many_receiver{this});
+  [[maybe_unused]] auto ignore_inactive = operation_states_of_children.enqueue(new_child);
   new_child->start();
+}
+
+template <typename SourceManySender, typename SenderFactory, typename ManyReceiver>
+operation_state<SourceManySender, SenderFactory, ManyReceiver>::operation_state(
+    SourceManySender&& source, SenderFactory&& f, ManyReceiver&& r)
+  : factory(std::move(f))
+  , many_receiver(std::move(r))
+  , parent_operation(
+        unifex::connect((SourceManySender &&) source, many_receiver_to_single_sender{this})) {
 }
 
 // A convenient typedef to strip cv qualifiers
@@ -207,6 +214,17 @@ using operation = operation_state<
 
 template <typename SourceManySender, typename SenderFactory>
 struct many_sender {
+  template <template <typename...> class Variant, template <typename...> class Tuple>
+  using value_types = Variant<Tuple<>>;
+
+  template <template <typename...> class Variant, template <typename...> class Tuple>
+  using next_types = Variant<Tuple<>>;
+
+  template <template <typename...> class Variant>
+  using error_types = Variant<>;
+
+  static constexpr bool sends_done = false;
+
   [[no_unique_address]] SourceManySender source;
   [[no_unique_address]] SenderFactory factory;
 
@@ -218,5 +236,18 @@ struct many_sender {
   }
 };
 
+constexpr inline struct fn {
+  template <typename ManySender, typename SenderFactory>
+  many_sender<std::remove_cvref_t<ManySender>, std::remove_cvref_t<SenderFactory>>
+  operator()(ManySender&& sender, SenderFactory&& factory) const noexcept {
+    static_assert(
+        unifex::detail::_has_sender_traits<
+            many_sender<std::remove_cvref_t<ManySender>, std::remove_cvref_t<SenderFactory>>>);
+    return {std::forward<ManySender>(sender), std::forward<SenderFactory>(factory)};
+  }
+} bulk_finally;
+
 }  // namespace bulk_finally_ns
+
+using bulk_finally_ns::bulk_finally;
 }  // namespace ampi
